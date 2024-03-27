0c11486eab81946a4db6df4beae20c3e2b9920aa
BrianSechelo <sechbrian123.gmail.com>
2024-03-27 22:55:10 +0300

    Task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,11 +7,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-if (tree == NULL)
-	return 0;
-size_t left_height;
-size_t right_height;
-left_height = binary_tree_height(tree->left);
-right_height = binary_tree_height(tree->right);
-return (left_height > right_height?left_height + 1: right_height + 1);
+	size_t left_height;
+	size_t right_height;
+	if (tree)
+	{
+		left_height = binary_tree_height(tree->left);
+		right_height = binary_tree_height(tree->right);
+		return (left_height > right_height?left_height + 1: right_height + 1);
+	}
+	return (0);
 }
