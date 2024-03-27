d9080c17784ada2df142a4698e404f8cefa87ad4
BrianSechelo <sechbrian123.gmail.com>
2024-03-27 22:45:04 +0300

    Task 7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -9,22 +9,11 @@
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
-		        return;
-	const binary_tree_t *current;
-	size_t stack_size;
-	const binary_tree_t *stack[1000];
-	current = tree;
-	stack_size = 0;
-	while (current != NULL || stack_size > 0)
-	{
-		while(current != NULL)
-		{
-		stack[stack_size++] = current;
-		current = current->left;
-		}
-		current = stack[--stack_size];
-		func(current->n);
-		current = current->right;
-	}
+		return;
 
+    	binary_tree_inorder(tree->left, func);
+
+        func(tree->n);
+
+	binary_tree_inorder(tree->right, func);
 }
